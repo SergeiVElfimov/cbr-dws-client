5acbbfeaa7daf50030202fb64c0a1015b1a86161
Sergey V. Elfimov <elfimovserg@gmail.com>
2025-06-20 16:22:36 +0300

    init commit

---
+++ tests/test_client.py
@@ -0,0 +1,30 @@
+from datetime import datetime, timedelta
+from decimal import Decimal
+
+from cbr_dws_client import CbrDwsClient
+
+
+class TestCbrDwsClient:
+    @classmethod
+    def setup_class(cls) -> None:
+        cls.cbr_dws_client = CbrDwsClient(verify=False)
+
+    def test_get_currencies_on_date(self):
+        res = self.cbr_dws_client.get_currencies_on_date(datetime.now())
+        assert isinstance(res, list)
+
+    def test_get_currencies_on_date_with_char_code(self):
+        res = self.cbr_dws_client.get_currencies_on_date(datetime.now(), "USD")
+        assert isinstance(res.Vcurs, Decimal)
+
+    def test_get_enum_currency_codes_list(self):
+        res = self.cbr_dws_client.get_enum_currency_codes(False)
+        assert isinstance(res, list)
+
+    def test_get_enum_currency_codes_list_with_char_code(self):
+        res = self.cbr_dws_client.get_enum_currency_codes(False, "USD")
+        assert isinstance(res.Vcode, str)
+
+    def test_get_currencies_dynamic_past_15_days(self):
+        res = self.cbr_dws_client.get_currencies_dynamic(datetime.now() - timedelta(days=15), datetime.now(), "USD")
+        assert isinstance(res, list)
